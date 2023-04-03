61be5417707afff725d5e0c5f8d1f8e6f0be665a
Bernars Kazoks <kazoksbernars@gmail.com>
2023-04-03 13:04:01 +0300

    register.html + registration controller + user model sample test

---
+++ src/main/java/com/bernarsk/onlinebanking/controllers/RegistrationController.java
@@ -0,0 +1,23 @@
+package com.bernarsk.onlinebanking.controllers;
+
+import com.bernarsk.onlinebanking.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class RegistrationController {
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new User());
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String processRegistrationForm(@ModelAttribute("user") User user) {
+        // Save user to database or perform other registration logic
+        return "redirect:/registration-success";
+    }
+}
