38bc1f2121088d6b747f6b1a59365510f1958d8c
Bernars Kazoks <kazoksbernars@gmail.com>
2023-04-03 14:04:31 +0300

    created UserRepository.java, updated User.java with DB connection, id field + RegistrationController.java autowire UserRepository

--- src/main/java/com/bernarsk/onlinebanking/controllers/RegistrationController.java
+++ src/main/java/com/bernarsk/onlinebanking/controllers/RegistrationController.java
@@ -1,6 +1,8 @@
 package com.bernarsk.onlinebanking.controllers;
 
 import com.bernarsk.onlinebanking.models.User;
+import com.bernarsk.onlinebanking.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class RegistrationController {
+    @Autowired private UserRepository userRepository;
+
     @GetMapping("/register")
     public String showRegistrationForm(Model model) {
         model.addAttribute("user", new User());
@@ -17,6 +21,15 @@ public class RegistrationController {
 
     @PostMapping("/register")
     public String processRegistrationForm(@ModelAttribute("user") User user) {
+        // Check if email address is already in use
+//        User existingUser = UserRepository.findByEmail(user.getEmail());
+////        if (existingUser != null) {
+////            model.addAttribute("errorMessage", "Email address is already in use");
+////            return "register";
+////        }
+
+        // Save user to database
+        userRepository.save(user);
         // Save user to database or perform other registration logic
         return "redirect:/registration-success";
     }
--- src/main/java/com/bernarsk/onlinebanking/models/User.java
+++ src/main/java/com/bernarsk/onlinebanking/models/User.java
@@ -1,8 +1,23 @@
 package com.bernarsk.onlinebanking.models;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String email;
     private String password;
+
+    public User() {}
+
+    public User(String password, String email) {
+        this.password = password;
+        this.email = email;
+    }
 
     public String getEmail() {
         return email;
