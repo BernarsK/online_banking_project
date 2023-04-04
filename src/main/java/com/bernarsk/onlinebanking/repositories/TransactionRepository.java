49341ad15c6289e81d42834a51742619cda20335
Bernars Kazoks <kazoksbernars@gmail.com>
2023-04-04 14:36:32 +0300

    changing database primary key to UUID

--- src/main/java/com/bernarsk/onlinebanking/repositories/TransactionRepository.java
+++ src/main/java/com/bernarsk/onlinebanking/repositories/TransactionRepository.java
@@ -3,8 +3,10 @@ import com.bernarsk.onlinebanking.models.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface TransactionRepository  extends JpaRepository<Transaction, Long>  {
+public interface TransactionRepository  extends JpaRepository<Transaction, UUID>  {
 
 
 }
--- src/main/java/com/bernarsk/onlinebanking/repositories/UserRepository.java
+++ src/main/java/com/bernarsk/onlinebanking/repositories/UserRepository.java
@@ -6,9 +6,11 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, UUID> {
     boolean existsByEmail(String email);
     User findByEmail(String email);
     @Query("SELECT u.userLevel FROM User u WHERE u.email = :email")
