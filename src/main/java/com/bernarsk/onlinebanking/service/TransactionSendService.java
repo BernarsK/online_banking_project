fd8258139403814cbae75cb669ab9208b0091b68
Oskars Stepanovs <RTL@inbox.lv>
2023-04-10 11:55:16 +0300

    Added processTransaction method that saves balance changes of accounts after transaction is saved and is approved

--- src/main/java/com/bernarsk/onlinebanking/service/TransactionSendService.java
+++ src/main/java/com/bernarsk/onlinebanking/service/TransactionSendService.java
@@ -18,20 +18,40 @@ public class TransactionSendService {
     private TransactionRepository transactionRepository;
     @Autowired
     private AccountRepository accountRepository;
+
     public void saveTransaction(HttpSession session, Transaction transaction) {
-        UUID userId = (UUID)session.getAttribute("UUID"); //logged in user id
+        UUID userId = (UUID) session.getAttribute("UUID"); //logged in user id
         Account senderAccount = accountRepository.findByAccountNumber(transaction.getAccountFrom());//account of sender
 //        List<Account> userAccounts = accountRepository.findByUserId(userId);
         if (userId.equals(senderAccount.getUserId())) //need to check if acountFrom is logged in user account
         {
-            if (accountRepository.existsByAccountNumber(transaction.getAccountTo()))
-            {
+            if (accountRepository.existsByAccountNumber(transaction.getAccountTo())) {
                 transaction.setDate(LocalDate.now());
                 if (transaction.getAmount() > 200)
                     transaction.setStatus_approved(0);
                 else transaction.setStatus_approved(1); //transactions with amount above 200 need approval
-                transactionRepository.save(transaction);
+                Transaction savedTransaction = transactionRepository.save(transaction);
+                if (savedTransaction.getStatus_approved() == 1) {
+                    // transaction was successfully saved and transaction is approved
+                    processTransaction(savedTransaction);
+                }
             } else ;//reciever account does not exist
+        }
+    }
+
+    private void processTransaction(Transaction transaction) {
+        if (transaction.getStatus_approved() == 1) {//processTransaction should be called olny on approved transactions but extra check in case of mistake
+            Account recieverAccount = accountRepository.findByAccountNumber(transaction.getAccountTo());
+            Account senderAccount = accountRepository.findByAccountNumber(transaction.getAccountFrom());
+            Double transactionAmount = transaction.getAmount();
+            Double senderBalance = senderAccount.getBalance();
+            Double recieverBalance = senderAccount.getBalance();
+            if (senderBalance - transactionAmount >= 0) { //check if sender has balance to cover transaction amount
+                recieverAccount.setBalance(recieverBalance);
+                accountRepository.save(recieverAccount);
+                senderAccount.setBalance(senderBalance - transactionAmount);
+                accountRepository.save(senderAccount);//change balance of accounts by transaction amount and save it to database
+            } else ;//not enough balance
         }
     }
 }
