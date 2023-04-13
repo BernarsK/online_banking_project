97c65ba0d5f09e30f5bfcc1dac074117274ade3e
Oskars Stepanovs <RTL@inbox.lv>
2023-04-13 12:46:15 +0300

    Added transaction filtering, by date, by reference, by status

--- src/main/java/com/bernarsk/onlinebanking/controllers/TransactionViewControler.java
+++ src/main/java/com/bernarsk/onlinebanking/controllers/TransactionViewControler.java
@@ -8,6 +8,7 @@ import com.bernarsk.onlinebanking.service.TransactionViewService;
 import com.bernarsk.onlinebanking.service.UserService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
@@ -40,8 +42,13 @@ public class TransactionViewControler {
         model.addAttribute("accounts", accounts);
         return "view-accounts";
     }
-    @PostMapping("/view-account-transactions")
-    public String processTransactionForm(@RequestParam("selectedAccount")String accountNumber, HttpSession session, Model model, RedirectAttributes redirectAttributes) {
+    @GetMapping("/view-account-transactions")
+    public String processTransactionForm(@RequestParam("selectedAccount")String accountNumber,
+                                         @RequestParam(name = "startDate", required = false)@DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
+                                         @RequestParam(name = "endDate", required = false)@DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate,
+                                         @RequestParam(name = "status", required = false) Integer status,
+                                         @RequestParam(name = "reference", required = false) String reference,
+                                         HttpSession session, Model model, RedirectAttributes redirectAttributes) {
         // call service class
         UUID userID = (UUID) session.getAttribute("UUID");
         // check if session exists
@@ -49,8 +56,20 @@ public class TransactionViewControler {
             redirectAttributes.addFlashAttribute("error", "not logged in");
             return "redirect:/login";
         }
+        if (endDate == null) { //if no end date provided default value is now
+            endDate = LocalDate.now();
+        }
         Account account = accountService.findByAccountNumber(accountNumber);
         List<Transaction> accountTransactions = transactionViewService.viewAccountTransactions(session, account, account);//can change later to look for incoming or outgoing transactions, curently looks for all
+        if (startDate != null) {
+            accountTransactions = transactionViewService.filterByDate(accountTransactions, startDate, endDate);
+        }
+        if (status != null) {
+            accountTransactions = transactionViewService.filterByStatus(accountTransactions, status);
+        }
+        if (reference != null) {
+            accountTransactions = transactionViewService.filterByReference(accountTransactions, reference);
+        }
         model.addAttribute("accountTransactions", accountTransactions);
         model.addAttribute("selectedAccount", accountNumber);
         return "view-account-transactions";
